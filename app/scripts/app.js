0353671dda430edb21cbff9692afcbfe7911fc56
Yadagiri Talada <ytalada@pkrmsezdc.com>
2016-01-04 23:32:14 +0530

    created skelton for the project

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nodeMongoResearchApp
+ * @description
+ * # nodeMongoResearchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nodeMongoResearchApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodeMongoResearchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nodeMongoResearchApp
+ */
+angular.module('nodeMongoResearchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
